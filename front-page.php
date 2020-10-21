a5ab5adecb26c95e0667d3f82c5f90220d186353
MrLister <rlister@wonkasoft.com>
2020-10-21 02:26:13 -0700

    fixing loop

--- front-page.php
+++ front-page.php
@@ -16,35 +16,22 @@ get_header(); ?>
 
 <main id="main" class="site-main" role="main">
 <?php
-// start loop.
-while ( have_posts() ) :
-	the_post();
-
 	// Include the page content template.
 	get_template_part( 'template-parts/page/content', 'home' );
-
-
-	// If comments are open or we have at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
-
-	// End of the loop.
-endwhile;
 ?>
 
 </main><!-- .site-main-->
 <?php
 
 $section_mods = new Wonkasoft_Starter_Section_Mods( 'front_page' );
-$sections = $section_mods->get_sections();
+$sections     = $section_mods->get_sections();
 if ( 0 < $section_mods->sections_qty ) :
 	for ( $i = 1; $i <= $section_mods->sections_qty; $i++ ) :
-		$section = $section_mods->get_sections()->{"section_$i"};
+		$section   = $section_mods->get_sections()->{"section_$i"};
 		$this_post = get_post( $section->page_id );
 		$set_color = 'style=background:' . $section->color . '; ';
-		$set_bg = ( ! empty( $section->img ) ) ? 'style=background-image:url("' . $section->img . '"); ' : $set_color;
-		$content = apply_filters( 'the_content', $this_post->post_content, $this_post->post_content );
+		$set_bg    = ( ! empty( $section->img ) ) ? 'style=background-image:url("' . $section->img . '"); ' : $set_color;
+		$content   = apply_filters( 'the_content', $this_post->post_content, $this_post->post_content );
 		?>
 <section id="section-<?php echo esc_attr( $i ); ?>" class="front-page-section <?php echo esc_attr( $this_post->post_name ); ?>" <?php echo esc_attr( $set_bg ); ?>>
 
@@ -54,4 +41,4 @@ if ( 0 < $section_mods->sections_qty ) :
 	endfor;
 endif;
 	get_footer();
-	
+
--- template-parts/page/content-home.php
+++ template-parts/page/content-home.php
@@ -10,102 +10,100 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="img-cta-wrap">
-	<?php
-	if ( has_post_thumbnail() ) :
-		wonkasoft_front_starter_post_thumbnail();
-	else :
-		?>
-		<div class="front-page-default-thumbnail">
-			<div class="circles-wrap">
-				<div class="first-circle"></div>
-				<div class="second-circle"></div>
-				<div class="third-outline-circle"></div>
-				<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/business-3370832.jpg' ); ?>" class="default-featured-image" />
-			</div>
+<div class="img-cta-wrap">
+<?php
+if ( has_post_thumbnail() ) :
+	wonkasoft_front_starter_post_thumbnail();
+else :
+	?>
+	<div class="front-page-default-thumbnail">
+		<div class="circles-wrap">
+			<div class="first-circle"></div>
+			<div class="second-circle"></div>
+			<div class="third-outline-circle"></div>
+			<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/business-3370832.jpg' ); ?>" class="default-featured-image" />
 		</div>
-	<?php endif; ?>
-	<?php
-	if ( ! empty( get_theme_mod( 'cta_message' ) ) ) :
-		$cta_title    = ( ! empty( get_theme_mod( 'cta_message' ) ) ) ? get_theme_mod( 'cta_message' ) : '';
-		$cta_link     = ( ! empty( get_theme_mod( 'cta_link' ) ) ) ? get_permalink( get_theme_mod( 'cta_link' ) ) : '';
-		$cta_btn_text = ( ! empty( get_theme_mod( 'cta_btn_text' ) ) ) ? get_theme_mod( 'cta_btn_text' ) : '';
-		?>
-		<div class="cta-message">
-			<header class="header-cta-message"><h4><?php echo wp_kses_post( $cta_title ); ?></h4>
-			</header>
-			<a href="<?php echo esc_url( $cta_link ); ?>" class="wonka-btn btn btn-primary"><?php echo wp_kses_post( $cta_btn_text ); ?></a>
-		</div>
-	<?php endif; ?>
 	</div>
-	<div class="entry-content">
-		<div class="inner-loop">
+<?php endif; ?>
+<?php
+if ( ! empty( get_theme_mod( 'cta_message' ) ) ) :
+	$cta_title    = ( ! empty( get_theme_mod( 'cta_message' ) ) ) ? get_theme_mod( 'cta_message' ) : '';
+	$cta_link     = ( ! empty( get_theme_mod( 'cta_link' ) ) ) ? get_permalink( get_theme_mod( 'cta_link' ) ) : '';
+	$cta_btn_text = ( ! empty( get_theme_mod( 'cta_btn_text' ) ) ) ? get_theme_mod( 'cta_btn_text' ) : '';
+	?>
+	<div class="cta-message">
+		<header class="header-cta-message"><h4><?php echo wp_kses_post( $cta_title ); ?></h4>
+		</header>
+		<a href="<?php echo esc_url( $cta_link ); ?>" class="wonka-btn btn btn-primary"><?php echo wp_kses_post( $cta_btn_text ); ?></a>
+	</div>
+<?php endif; ?>
+</div>
+<div class="entry-content">
+	<div class="inner-loop">
+		<?php
+		$query = new WP_Query(
+			array(
+				'post_type'      => array( 'page' ),
+				'orderby'        => array( 'menu_order' ),
+				'order'          => array( 'ASC' ),
+				'posts_per_page' => 3,
+			)
+		);
+		if ( $query->have_posts() ) :
+			?>
+
 			<?php
-			$query = new WP_Query(
+
+			/* Start the Loop */
+			while ( $query->have_posts() ) :
+				$query->the_post();
+
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/inner', get_post_format() );
+
+			endwhile;
+			wp_reset_postdata();
+			the_posts_navigation();
+
+			else :
+
+				get_template_part( 'template-parts/inner', 'none' );
+
+		endif;
+			the_content();
+
+			wp_link_pages(
 				array(
-					'post_type'      => array( 'page' ),
-					'orderby'        => array( 'menu_order' ),
-					'order'          => array( 'ASC' ),
-					'posts_per_page' => 3,
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wonkasoft-starter' ),
+					'after'  => '</div>',
 				)
 			);
-			if ( $query->have_posts() ) :
-				?>
-
-				<?php
-
-				/* Start the Loop */
-				while ( $query->have_posts() ) :
-					$query->the_post();
-
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/inner', get_post_format() );
-
-				endwhile;
-				wp_reset_postdata();
-				the_posts_navigation();
-
-				else :
-
-					get_template_part( 'template-parts/inner', 'none' );
-
-			endif;
-				the_content();
-
-				wp_link_pages(
-					array(
-						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wonkasoft-starter' ),
-						'after'  => '</div>',
-					)
-				);
-				?>
-		</div><!-- .inner-loop -->
-	</div><!-- .entry-content -->
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-				edit_post_link(
-					sprintf(
-						wp_kses(
-							/* translators: %s: Name of current post. Only visible to screen readers */
-							__( 'Edit Page <span class="screen-reader-text">%s</span>', 'wonkasoft-starter' ),
-							array(
-								'span' => array(
-									'class' => array(),
-								),
-							)
-						),
-						get_the_title()
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
 			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+	</div><!-- .inner-loop -->
+</div><!-- .entry-content -->
+<?php if ( get_edit_post_link() ) : ?>
+	<footer class="entry-footer">
+		<?php
+			edit_post_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Edit Page <span class="screen-reader-text">%s</span>', 'wonkasoft-starter' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+		?>
+	</footer><!-- .entry-footer -->
+<?php endif; ?>
